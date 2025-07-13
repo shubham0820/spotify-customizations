cfcb540461f8ae538bb464396f25df60fc26edb7
shubham <shubhammishra94@gmail.com>
2025-07-13 12:41:24 +0530

    [spotify-customizations] : configuring the callback to

---
+++ src/main/java/com/cactro/spotify_customizations/controller/App.java
@@ -0,0 +1,60 @@
+package com.cactro.spotify_customizations.controller;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+@RestController
+public class App {
+    @Value("${spotify-customizations.client.clientId")
+    private String clientId;
+
+    @Value("${spotify-customizations.client.clientSecret")
+    private String clientSecret;
+
+    @Value("${spotify-customizations.client.redirectUri")
+    private String redirectUri;
+
+    private static final Logger logger = LogManager.getLogger(App.class);
+    @GetMapping("/callback")
+    public ResponseEntity<?> callback(@RequestParam String code) {
+
+        logger.debug("code: " + code);
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+        headers.setBasicAuth(clientId, clientSecret); // Base64(clientId:clientSecret)
+
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("grant_type", "authorization_code");
+        body.add("code", code);
+        body.add("redirect_uri", redirectUri);
+
+        HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(body, headers);
+
+        ResponseEntity<Map> response = restTemplate.postForEntity(
+                "https://accounts.spotify.com/api/token", request, Map.class
+        );
+
+        Map<String, Object> responseBody = response.getBody();
+        String accessToken = (String) responseBody.get("access_token");
+
+        // TODO: Save token in session, cookie, or DB
+        return ResponseEntity.ok(responseBody);
+    }
+
+}
