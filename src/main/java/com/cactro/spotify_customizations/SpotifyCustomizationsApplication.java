93b1a14b9c8380d613dbaf0f8f1e155360a25b60
shubham <shubhammishra94@gmail.com>
2025-07-13 11:23:33 +0530

    first commit

---
+++ src/main/java/com/cactro/spotify_customizations/SpotifyCustomizationsApplication.java
@@ -0,0 +1,13 @@
+package com.cactro.spotify_customizations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyCustomizationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifyCustomizationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cactro/spotify_customizations/SpotifyCustomizationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cactro.spotify_customizations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifyCustomizationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
